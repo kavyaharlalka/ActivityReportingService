fd122cb1c56f9dd4f03ec25eb4c46c90c4769c12
kavyaharlalka <kavyaharlalka@gmail.com>
2021-05-12 17:00:07 +0530

    Add main

---
+++ ActivityReportingService/BusinessLayer/ActivityEventBusinessLayer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ActivityReportingService.BusinessLayer
+{
+    public class ActivityEventBusinessLayer : IActivityEventBusinessLayer
+    {
+        private static ConcurrentDictionary<string, ActivityEventHandler> _activityKeyToActivityHandlerMap = new ConcurrentDictionary<string, ActivityEventHandler>();
+
+        public void AddActivityEvent(string key, int value, DateTime timeStamp)
+        {
+            ActivityEventHandler activityEventHandler;
+            if (!_activityKeyToActivityHandlerMap.TryGetValue(key, out activityEventHandler))
+            {
+                activityEventHandler = new ActivityEventHandler();
+                _activityKeyToActivityHandlerMap[key] = activityEventHandler;
+            }
+
+            activityEventHandler.AddActivityEvent(value, timeStamp);
+        }
+
+        public int GetActivityEventTotal(string key)
+        {
+            ActivityEventHandler activityEventHandler;
+            if (!_activityKeyToActivityHandlerMap.TryGetValue(key, out activityEventHandler))
+            {
+                return -1;
+            }
+
+            return activityEventHandler.GetTotal();
+        }
+    }
+}
